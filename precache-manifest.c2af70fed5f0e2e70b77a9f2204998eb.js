a8d1a1cbe5c10971e273f9b0d20f4b1b6642468d
aFedorovich <a.fedorovich@pixelplex.io>
2020-06-09 16:41:34 +0300

    test

---
+++ precache-manifest.c2af70fed5f0e2e70b77a9f2204998eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa4e107503a2da758c6adc8132dfba9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48125196312196e58d40",
+    "url": "/static/css/main.c3d3e946.chunk.css"
+  },
+  {
+    "revision": "ef4c3473fbd65ffc4228",
+    "url": "/static/js/2.e1b4729d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1b4729d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48125196312196e58d40",
+    "url": "/static/js/main.88a57d41.chunk.js"
+  },
+  {
+    "revision": "b38f709691f005f4c585",
+    "url": "/static/js/runtime-main.e2af1984.js"
+  }
+]);
